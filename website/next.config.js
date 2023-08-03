c338618958f38b2d65666d285bd9b3a44c151d52
emilkowalski <e@emilkowal.ski>
2023-08-04 00:25:34 +0200

    Add

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'emilkowal.ski',
+      },
+    ],
+  },
+};
--- website/src/app/components/hero.tsx
+++ website/src/app/components/hero.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import { Drawer } from 'vaul';
 
@@ -44,7 +45,7 @@ export function Hero() {
         </div>
         <div className="flex gap-4 justify-center mt-6">
           <Drawer.Root
-            snapPoints={pathname.includes('test') ? undefined : [0.2, 0.5, 1]}
+            snapPoints={pathname.includes('test') ? undefined : [0.4, 1]}
             onSnapPointChange={(snap) => console.log(snap)}
           >
             <Drawer.Trigger asChild>
@@ -57,36 +58,140 @@ export function Hero() {
             </Drawer.Trigger>
             <Drawer.Overlay className="fixed inset-0 bg-black/40" />
             <Drawer.Portal>
-              <Drawer.Content className="bg-white border border-gray-200 flex flex-col fixed bottom-0 left-0 right-0 max-h-[85vh] rounded-t-[10px]">
-                <div className="max-w-md w-full mx-auto flex flex-col overflow-auto p-4 rounded-t-[10px]">
-                  <input className="border border-gray-400 my-8" placeholder="Input" />
-                  <p>
-                    But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was
-                    born and I will give you a complete account of the system, and expound the actual teachings of the
-                    great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or
-                    avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue
-                    pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who
-                    loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally
-                    circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial
-                    example, which of us ever undertakes laborious physical exercise, except to obtain some advantage
-                    from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no
-                    annoying consequences, or one who avoids a pain that produces no resultant pleasure?
-                  </p>
-                  <input className="border border-gray-400 my-8" placeholder="Input" />
-                  <p>
-                    On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
-                    demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee
-                    the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their
-                    duty through weakness of will, which is the same as saying through shrinking from toil and pain.
-                    These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice
-                    is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is
-                    to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty
-                    or the obligations of business it will frequently occur that pleasures have to be repudiated and
-                    annoyances accepted. The wise man therefore always holds in these matters to this principle of
-                    selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid
-                    worse pains.
-                  </p>
-                  <input className="border border-gray-400 my-8" placeholder="Input" />
+              <Drawer.Content className="bg-gray-100 border border-gray-200 flex flex-col fixed bottom-0 left-0 right-0 max-h-[85vh] rounded-t-[10px]">
+                <header className="p-4 flex items-center gap-3 border-b border-gray-200">
+                  <Image
+                    alt="Emil's picture"
+                    src="https://emilkowal.ski/emil-kowalski.jpg"
+                    width={32}
+                    height={32}
+                    className="rounded-full"
+                  />
+                  <div className="flex flex-col grow gap-1">
+                    <span className="leading-none text-sm font-medium">Emil Kowalski</span>
+                    <span className="text-gray-600 text-xs leading-none">emilkowal.ski</span>
+                  </div>
+                  <Drawer.Close className="h-6 w-6 bg-gray-200 flex items-center justify-center rounded-full text-gray-500">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={2}
+                      stroke="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </Drawer.Close>
+                </header>
+                <div className="flex p-4 gap-4">
+                  <div>
+                    <div className="h-[52px] w-[52px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-full" />
+                    <span className="text-xs block text-center mt-1">Jack</span>
+                  </div>
+                  <div>
+                    <div className="h-[52px] w-[52px] bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-full" />
+                    <span className="text-xs block text-center mt-1">Lucia</span>
+                  </div>
+                  <div>
+                    <div className="h-[52px] w-[52px] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full" />
+                    <span className="text-xs block text-center mt-1">Marta</span>
+                  </div>
+                  <div>
+                    <div className="h-[52px] w-[52px] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full" />
+                    <span className="text-xs block text-center mt-1">Marta</span>
+                  </div>
+                  <div>
+                    <div className="h-[52px] w-[52px] bg-gradient-to-r from-cyan-500 to-blue-500 rounded-full" />
+                    <span className="text-xs block text-center mt-1">Marta</span>
+                  </div>
+                </div>
+                <div className="p-4">
+                  <div className="flex flex-col py-2.5">
+                    <button className="h-12 bg-white text-sm text-left px-3 flex justify-between items-center">
+                      Copy{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="flex flex-col my-1 bg-white rounded-lg divide-y divide-gray-200">
+                    <button className="h-12 flex justify-between items-center text-sm text-left px-3">
+                      Add Bookmark{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                        />
+                      </svg>
+                    </button>
+                    <button className="h-12 flex items-center justify-between text-sm text-left px-3">
+                      Add to Favourites{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                        />
+                      </svg>
+                    </button>
+                    <button className="h-12 flex items-center justify-between text-left px-3">
+                      Find on Page{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
+                        />
+                      </svg>
+                    </button>
+                    <button className="h-12 flex justify-between items-center text-sm text-left px-3">
+                      Add to Home Screen{' '}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                      </svg>
+                    </button>
+                  </div>
                 </div>
               </Drawer.Content>
             </Drawer.Portal>
