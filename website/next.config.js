68589d1b9a32ca4ea60fe124733d4632c4f3c349
emilkowalski <e@emilkowal.ski>
2023-09-02 11:09:40 +0200

    Start working on docs

--- website/next.config.js
+++ website/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.jsx',
+});
 
-module.exports = nextConfig;
+module.exports = withNextra();
---
+++ website/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span style={{ fontWeight: 600 }}>Vaul</span>,
+  project: {
+    link: 'https://github.com/emilkowalski/vaul',
+  },
+  // ... other theme options
+};
