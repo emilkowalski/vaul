2f796f119ecc832293186c6cb49d6138ef76f355
emilkowalski <e@emilkowal.ski>
2023-09-02 11:25:34 +0200

    test

--- website/src/app/components/hero.tsx
+++ website/src/app/components/hero.tsx
@@ -1,10 +1,128 @@
 'use client';
 
 import { Drawer } from 'vaul';
+import { Sandpack } from '@codesandbox/sandpack-react';
 
 export function Hero() {
   return (
     <div className="relative">
+      <Sandpack
+        template="react"
+        customSetup={{
+          dependencies: {
+            vaul: 'latest',
+          },
+        }}
+        files={{
+          '/App.tsx': `import { Drawer } from 'vaul';
+
+		  export default function MyDrawer() {
+			return (
+			  <Drawer.Root shouldScaleBackground>
+				<Drawer.Trigger asChild>
+				  <button>Open Drawer</button>
+				</Drawer.Trigger>
+				<Drawer.Portal>
+				  <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+				  <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
+					<div className="p-4 bg-white rounded-t-[10px] flex-1">
+					  <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+					  <div className="max-w-md mx-auto">
+						<Drawer.Title className="font-medium mb-4">
+						  Unstyled drawer for React.
+						</Drawer.Title>
+						<p className="text-zinc-600 mb-2">
+						  This component can be used as a replacement for a Dialog on
+						  mobile and tablet devices.
+						</p>
+						<p className="text-zinc-600 mb-8">
+						  It uses{" "}
+						  <a
+							href="https://www.radix-ui.com/docs/primitives/components/dialog"
+							className="underline"
+							target="_blank"
+						  >
+							Radix&apos;s Dialog primitive
+						  </a>{" "}
+						  under the hood and is inspired by{" "}
+						  <a
+							href="https://twitter.com/devongovett/status/1674470185783402496"
+							className="underline"
+							target="_blank"
+						  >
+							this tweet.
+						  </a>
+						</p>
+					  </div>
+					</div>
+					<div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
+					  <div className="flex gap-6 justify-end max-w-md mx-auto">
+						<a
+						  className="text-xs text-zinc-600 flex items-center gap-0.25"
+						  href="https://github.com/emilkowalski/vaul"
+						  target="_blank"
+						>
+						  GitHub
+						  <svg
+							fill="none"
+							height="16"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							width="16"
+							aria-hidden="true"
+							className="w-3 h-3 ml-1"
+						  >
+							<path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+							<path d="M15 3h6v6"></path>
+							<path d="M10 14L21 3"></path>
+						  </svg>
+						</a>
+						<a
+						  className="text-xs text-zinc-600 flex items-center gap-0.25"
+						  href="https://twitter.com/emilkowalski_"
+						  target="_blank"
+						>
+						  Twitter
+						  <svg
+							fill="none"
+							height="16"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							width="16"
+							aria-hidden="true"
+							className="w-3 h-3 ml-1"
+						  >
+							<path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+							<path d="M15 3h6v6"></path>
+							<path d="M10 14L21 3"></path>
+						  </svg>
+						</a>
+					  </div>
+					</div>
+				  </Drawer.Content>
+				</Drawer.Portal>
+			  </Drawer.Root>
+			);
+		  }`,
+          '/node_modules/vaul/package.json': {
+            hidden: true,
+            code: JSON.stringify({
+              name: 'vaul',
+              main: './index.js',
+            }),
+          },
+          '/node_modules/vaul/index.js': {
+            hidden: true,
+            code: Drawer,
+          },
+        }}
+      />
       <div
         aria-hidden
         className="absolute top-0 w-[1000px] z-10 h-[400px] left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-[0.15] pointer-events-none"
