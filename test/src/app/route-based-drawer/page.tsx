d31ff939e7d0dbf7b06126334b914da2ca455169
Kajetan Szymczak <kajtszymczak@gmail.com>
2023-10-13 13:26:06 +0200

    Make route based drawer example more clear and add workaround to fix issue

--- test/src/app/route-based-drawer/page.tsx
+++ test/src/app/route-based-drawer/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import clsx from 'clsx';
 import { useRouter } from 'next/navigation';
 import { useEffect, useLayoutEffect, useState } from 'react';
 import { Drawer } from 'vaul';
@@ -7,6 +8,7 @@ import { Drawer } from 'vaul';
 export default function Page() {
   const router = useRouter();
   const [snap, setSnap] = useState<number | null | string>(0.4);
+  const [, setOpen] = useState(false);
 
   const setActiveSnapPoint = (snap: number | string | null) => {
     if (snap === 0) {
@@ -16,172 +18,143 @@ export default function Page() {
     setSnap(snap);
   };
 
+  useEffect(() => {
+    window.setTimeout(() => {
+      setOpen(true);
+    }, 200);
+  }, []);
+
   return (
-    <div className="w-screen min-h-screen overflow-auto bg-white p-8" vaul-drawer-wrapper="">
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit. Esse cillum dolore eu fugiat nulla pariatur. Neque
-        porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam
-        est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.
-      </p>
-      <p>
-        Animi, id est laborum et dolorum fuga. Facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-        Qui officia deserunt mollit anim id est laborum. Animi, id est laborum et dolorum fuga. Eaque ipsa quae ab illo
-        inventore veritatis et quasi.
-      </p>
-      <p>
-        Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nihil molestiae consequatur, vel
-        illum qui dolorem eum. Animi, id est laborum et dolorum fuga. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa.
-      </p>
-      <p>
-        Architecto beatae vitae dicta sunt explicabo. Duis aute irure dolor in reprehenderit in voluptate velit. Nemo
-        enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit. Qui officia deserunt mollit anim id est
-        laborum.
-      </p>
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit. Totam rem aperiam. Temporibus autem quibusdam et aut
-        officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non
-        recusandae.
-      </p>
-      <p>
-        Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam. Itaque earum rerum hic tenetur a sapiente delectus. Quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut enim ad minima veniam, quis nostrum exercitationem
-        ullam corporis suscipit laboriosam. Qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde
-        omnis iste natus error sit voluptatem.
-      </p>
-      <p>
-        Accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo. Duis aute irure dolor in
-        reprehenderit in voluptate velit. Et harum quidem rerum facilis est et expedita distinctio. Lorem ipsum dolor
-        sit amet, consectetur adipisicing elit.
-      </p>
-      <p>
-        Eaque ipsa quae ab illo inventore veritatis et quasi. Neque porro quisquam est, qui dolorem ipsum quia dolor sit
-        amet, consectetur, adipisci velit. Totam rem aperiam. Animi, id est laborum et dolorum fuga.
-      </p>
-      <p>
-        Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates
-        repudiandae sint et molestiae non recusandae. Et iusto odio dignissimos ducimus qui blanditiis praesentium
-        voluptatum deleniti atque.
-      </p>
-      <p>
-        Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco. Animi, id est laborum et dolorum fuga. Animi, id est laborum et dolorum fuga.
-      </p>
-      <p>
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco. Temporibus autem quibusdam et aut officiis debitis
-        aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae.
-      </p>
-      <p>
-        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Nisi ut aliquid ex
-        ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.
-      </p>
-      <p>
-        Cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia. Quia consequuntur magni dolores
-        eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-        consectetur, adipisci velit.
-      </p>
-      <p>
-        Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium doloremque laudantium, totam rem
-        aperiam, eaque ipsa quae ab illo.
-      </p>
-      <p>
-        Laboris nisi ut aliquip ex ea commodo consequat. Ut aut reiciendis voluptatibus maiores alias consequatur aut
-        perferendis doloribus asperiores repellat. Laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
-      <p>
-        Nihil molestiae consequatur, vel illum qui dolorem eum. Sed ut perspiciatis unde omnis iste natus error sit
-        voluptatem. Nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate
-        velit esse quam.
-      </p>
-      <p>
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco. Et iusto odio dignissimos ducimus qui blanditiis
-        praesentium voluptatum deleniti atque. Facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.
-      </p>
-      <p>
-        Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Ut enim ad minima
-        veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam. Inventore veritatis et quasi architecto
-        beatae vitae dicta sunt explicabo.
-      </p>
-      <p>
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa. Corrupti quos dolores et quas molestias excepturi sint occaecati. Quia consequuntur magni dolores
-        eos qui ratione voluptatem sequi nesciunt.
-      </p>
-      <Drawer.Root
-        open={true}
-        onOpenChange={() => {}}
-        snapPoints={[0, 0.4, 1]}
-        activeSnapPoint={snap}
-        setActiveSnapPoint={setActiveSnapPoint}
-      >
+    <div className="w-screen h-screen bg-white p-8 flex justify-center items-center">
+      <div data-testid="active-snap-index">{snap}</div>
+      <Drawer.Root open snapPoints={[0, 0.4, 1]} activeSnapPoint={snap} setActiveSnapPoint={setActiveSnapPoint}>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
         <Drawer.Portal>
-          <Drawer.Overlay className="fixed inset-0 bg-black/40" />
           <Drawer.Content
             data-testid="content"
-            className="bg-zinc-100 flex flex-col rounded-t-[10px] mt-24 fixed bottom-0 left-0 right-0 outline-none"
+            className="fixed flex flex-col bg-white border border-gray-200 border-b-none rounded-t-[10px] bottom-0 left-0 right-0 h-full max-h-[97%] mx-[-1px]"
           >
-            <div className="p-4 bg-white rounded-t-[10px] flex-1">
-              <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
-              <div className="max-w-md mx-auto">
-                <Drawer.Title className="font-medium mb-4">
-                  This drawer opens when users navigates to <i>/route-based-drawer</i>
-                </Drawer.Title>
-                <p>To close drawer just swipe down</p>
+            <div
+              className={clsx('flex flex-col max-w-md mx-auto w-full p-4 pt-5', {
+                'overflow-y-auto': snap === 1,
+                'overflow-hidden': snap !== 1,
+              })}
+            >
+              <div className="flex items-center">
+                <svg
+                  className="text-yellow-400 h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  className="text-yellow-400 h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  className="text-yellow-400 h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  className="text-yellow-400 h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+                <svg
+                  className="text-gray-300 h-5 w-5 flex-shrink-0"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>{' '}
+              <h1 className="text-2xl mt-2 font-medium">The Hidden Details</h1>
+              <p className="text-sm mt-1 text-gray-600 mb-6">2 modules, 27 hours of video</p>
+              <p className="text-gray-600">
+                The world of user interface design is an intricate landscape filled with hidden details and nuance. In
+                this course, you will learn something cool. To the untrained eye, a beautifully designed UI.
+              </p>
+              <button className="bg-black text-gray-50 mt-8 rounded-md h-[48px] flex-shrink-0 font-medium">
+                Buy for $199
+              </button>
+              <div className="mt-12">
+                <h2 className="text-xl font-medium">Module 01. The Details</h2>
+                <div className="space-y-4 mt-4">
+                  <div>
+                    <span className="block">Layers of UI</span>
+                    <span className="text-gray-600">A basic introduction to Layers of Design.</span>
+                  </div>
+                  <div>
+                    <span className="block">Typography</span>
+                    <span className="text-gray-600">The fundamentals of type.</span>
+                  </div>
+                  <div>
+                    <span className="block">UI Animations</span>
+                    <span className="text-gray-600">Going through the right easings and durations.</span>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
-              <div className="flex gap-6 justify-end max-w-md mx-auto">
-                <a
-                  className="text-xs text-zinc-600 flex items-center gap-0.25"
-                  href="https://github.com/emilkowalski/vaul"
-                  target="_blank"
-                >
-                  GitHub
-                  <svg
-                    fill="none"
-                    height="16"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    width="16"
-                    aria-hidden="true"
-                    className="w-3 h-3 ml-1"
-                  >
-                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                    <path d="M15 3h6v6"></path>
-                    <path d="M10 14L21 3"></path>
-                  </svg>
-                </a>
-                <a
-                  className="text-xs text-zinc-600 flex items-center gap-0.25"
-                  href="https://twitter.com/emilkowalski_"
-                  target="_blank"
-                >
-                  Twitter
-                  <svg
-                    fill="none"
-                    height="16"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    width="16"
-                    aria-hidden="true"
-                    className="w-3 h-3 ml-1"
-                  >
-                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
-                    <path d="M15 3h6v6"></path>
-                    <path d="M10 14L21 3"></path>
-                  </svg>
-                </a>
+              <div className="mt-12">
+                <figure>
+                  <blockquote className="font-serif">
+                    “I especially loved the hidden details video. That was so useful, learned a lot by just reading it.
+                    Can&rsquo;t wait for more course content!”
+                  </blockquote>
+                  <figcaption>
+                    <span className="text-sm text-gray-600 mt-2 block">Yvonne Ray, Frontend Developer</span>
+                  </figcaption>
+                </figure>
+              </div>
+              <div className="mt-12">
+                <h2 className="text-xl font-medium">Module 02. The Process</h2>
+                <div className="space-y-4 mt-4">
+                  <div>
+                    <span className="block">Build</span>
+                    <span className="text-gray-600">Create cool components to practice.</span>
+                  </div>
+                  <div>
+                    <span className="block">User Insight</span>
+                    <span className="text-gray-600">Find out what users think and fine-tune.</span>
+                  </div>
+                  <div>
+                    <span className="block">Putting it all together</span>
+                    <span className="text-gray-600">Let&apos;s build an app together and apply everything.</span>
+                  </div>
+                </div>
               </div>
             </div>
           </Drawer.Content>
