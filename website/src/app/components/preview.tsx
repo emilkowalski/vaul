5c29a287e56328fab5f1b03a768ebb92863d5fea
Emil Kowalski <e@emilkowal.ski>
2024-09-18 19:00:44 +0200

    wip

--- website/src/app/components/preview.tsx
+++ website/src/app/components/preview.tsx
@@ -1,11 +1,12 @@
 import { Code } from './code';
 
 export function Preview(props) {
-  const code = `\`${props.source}\``;
+  const code = `\`\`\`
+${props.source}
+  \`\`\``;
 
   return (
     <div>
-      {props.source}
       <Code code={code} />
     </div>
   );
