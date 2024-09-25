e5a27842db2da23fab372b8395f588faf3d51126
Emil Kowalski <e@emilkowal.ski>
2024-09-26 00:10:32 +0200

    test

--- website/next.config.js
+++ website/next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: 'https://vaul-docs.vercel.app',
   async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: 'https://vaul-docs.vercel.app/:path*',
-      },
-    ];
+    return {
+      fallback: [
+        {
+          source: '/:path*',
+          destination: `https://vaul-docs.vercel.app/:path*`,
+        },
+      ],
+    };
   },
 };
 
