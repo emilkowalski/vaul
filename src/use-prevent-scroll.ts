fb2b9fd335a22b7537e6a25c4470d4901a31c7ab
maiconcarraro <maicon315@gmail.com>
2024-10-07 01:44:12 -0300

    fix: restore original code

--- src/use-prevent-scroll.ts
+++ src/use-prevent-scroll.ts
@@ -267,12 +267,15 @@ function preventScrollMobileSafari() {
 }
 
 // Sets a CSS property on an element, and returns a function to revert it to the previous value.
-function setStyle(element: HTMLElement, style: string, value: string) {
-  let cur = element.style.getPropertyValue(style);
-  element.style.setProperty(style, value);
+function setStyle(element: HTMLElement, style: keyof React.CSSProperties, value: string) {
+  // https://github.com/microsoft/TypeScript/issues/17827#issuecomment-391663310
+  let elStyle = <any>element.style;
+
+  let cur = elStyle[style];
+  elStyle[style] = value;
 
   return () => {
-    element.style.setProperty(style, cur);
+    elStyle[style] = cur;
   };
 }
 
